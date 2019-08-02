ca37e66e3437907b2cdda708521f01ae60f21c71
Hanani-Chen <40013556+Hanani-Chen@users.noreply.github.com>
2019-08-02 19:14:39 +0800

    Update geometry.cpp

--- geometry.cpp
+++ geometry.cpp
@@ -129,3 +129,37 @@ int main() {
 	printf("%.10f\n%.10f %.10f",R,ans.x,ans.y);
 }
 /******************************************************/
+
+
+最小球覆盖模拟退火(别乱用)
+/**********************************/
+struct point3D
+{
+    double x,y,z;
+} data[35];
+int n;
+double dis(point3D a,point3D b)
+{
+    return sqrt((a.x-b.x)*(a.x-b.x)+(a.y-b.y)*(a.y-b.y)+(a.z-b.z)*(a.z-b.z));
+}
+double solve()
+{
+    double step=1000,ans=1e30,mt;
+    point3D z;
+    z.x=z.y=z.z=0;
+    int s=0;
+    while(step>eps)
+    {
+        for(int i=0; i<n; i++)
+            if(dis(z,data[s])<dis(z,data[i])) s=i;
+        mt=dis(z,data[s]);
+        ans=min(ans,mt);
+        z.x+=(data[s].x-z.x)/mt*step;
+        z.y+=(data[s].y-z.y)/mt*step;
+        z.z+=(data[s].z-z.z)/mt*step;
+       // dd(step);dd(z.x);dd(z.y);de(z.z);
+        step*=0.98;
+    }
+    return ans;
+}
+/**********************************/

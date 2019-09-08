3642305bb4e8e0598edcbf98daecc5e5c638c07a
Hanani-Chen <40013556+Hanani-Chen@users.noreply.github.com>
2019-09-08 21:07:19 +0800

    Update data structure.cpp

--- data structure.cpp
+++ data structure.cpp
@@ -397,3 +397,110 @@ int main() {
 	}
 }
 /*********************************************/
+
+
+
+//Tarjan算法 复杂度O(n+Q)
+/***************************/
+const int MAXN=5e5+5;
+const int MAXQ=5e5+5;
+int F[MAXN];
+int find(int x) {
+	if(F[x]==-1) return x;
+	return F[x]=find(F[x]);
+}
+void bing(int u,int v) {
+	int t1=find(u);
+	int t2=find(v);
+	if(t1!=t2) F[t1]=t2;
+}
+
+bool vis[MAXN];
+int ancestor[MAXN];
+struct Edge{
+	int to,next;
+}edge[MAXN*2];
+int head[MAXN],tot;
+void addedge(int u,int v) {
+	edge[tot].to=v;
+	edge[tot].next=head[u];
+	head[u]=tot++;
+}
+struct Query {
+	int q,next;
+	int index;
+}query[MAXQ*2];
+int answer[MAXQ];
+int h[MAXQ];
+int tt;
+int Q;
+void add_query(int u,int v,int index) {
+	query[tt].q=v;
+	query[tt].next=h[u];
+	query[tt].index=index;
+	h[u]=tt++;
+	query[tt].q=u;
+	query[tt].next=h[v];
+	query[tt].index=index;
+	h[v]=tt++;
+}
+void init() {
+	tot=0;
+	mem(head,-1);
+	tt=0;
+	mem(h,-1);
+	mem(vis,0);
+	mem(F,-1);
+	mem(ancestor,0);
+}
+void LCA(int u) {
+	ancestor[u]=u;
+	vis[u]=true;
+	for(int i=head[u];i!=-1;i=edge[i].next) {
+		int v=edge[i].to;
+		if(vis[v])continue;
+		LCA(v);
+		bing(u,v);
+		ancestor[find(u)]=u;
+	}
+	for(int i=h[u];i!=-1;i=query[i].next) {
+		int v=query[i].q;
+		if(vis[v]) {
+			answer[query[i].index]=ancestor[find(v)];
+		}
+	}
+}
+bool flag[MAXN];
+int main() {
+	int n;
+	int u,v,k;
+	int t;
+	//scanf("%d",&t);
+	t=1;
+	ff(ii,1,t) {
+		int root;
+		int k;
+		//printf("Case %d:\n",ii);
+		init();
+		mem(flag,0);
+		scanf("%d%d%d",&n,&k,&root);
+		ff(i,1,n-1) {
+			scanf("%d%d",&u,&v);
+			addedge(u,v);
+			addedge(v,u);
+		}
+		ff(i,1,k) {
+			scanf("%d%d",&u,&v);
+		//	dd(u);de(v);
+			add_query(u,v,i);
+		}
+		/*
+		ff(i,1,n) if(!flag[i]) {
+			root=i;
+			break;
+		}*/
+		LCA(root);
+		ff(i,1,k) printf("%d\n",answer[i]);
+	}	
+}
+/***************************/
